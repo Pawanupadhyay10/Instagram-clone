28bbabd47a8c947452d5cbd6a1803fa5e7380d21
pawan10 <pawandev1099@gmail.com>
2021-05-24 21:29:43 +0530

    models

---
+++ models/users.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const userSchema=new mongoose.Schema({
+    name:{type:String,required:true},
+    email:{ type: String, required: true},
+    passward:{ type: String, required: true }
+})
+
+mongoose.model("User",userSchema)
