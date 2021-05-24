8a72d86c1c087335514487825bb70efdd4867597
pawan10 <pawandev1099@gmail.com>
2021-05-24 21:25:16 +0530

    app

---
+++ app.js
@@ -0,0 +1,28 @@
+const express=require('express')
+const app=express()
+const mongoose=require('mongoose')
+const PORT=3000
+const{MONGOURI}=require('./keys')
+//9sZBODpeAEMomtoJ
+
+
+
+mongoose.connect(MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connection.on('connected',()=>{
+    console.log("Mongo connected:");
+})
+
+
+mongoose.connection.on('error',(error) => {
+    console.log("error connected:",error);
+})
+
+require('./models/users')
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+
+
+app.listen(PORT,()=>{
+console.log("localhost::",PORT);
+})
