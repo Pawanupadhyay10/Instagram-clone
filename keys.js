1cf0adcf271ad1194af6a0f35e2e606451badd02
Pawanupadhyay10 <52281814+Pawanupadhyay10@users.noreply.github.com>
2021-05-26 10:13:55 +0530

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
 module.exports={
-    MONGOURI:"mongodb+srv://pawan:9sZBODpeAEMomtoJ@cluster0.xj4kf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    MONGOURI:"mongodb+srv://pawan:<passward>@cluster0.xj4kf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     ,JWT_SECRET:"efefefefefefe"
 }
