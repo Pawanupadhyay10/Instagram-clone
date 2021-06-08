f7b71c5ca60e9deb5f0ad8c299c9bf54bce04e5c
Pawanupadhyay10 <52281814+Pawanupadhyay10@users.noreply.github.com>
2021-06-08 14:17:47 +0530

    Delete keys.js

--- keys.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-    MONGOURI:"mongodb+srv://amrita:hEcF6q9QUuuExkJY@cluster0.xlwbk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-    ,JWT_SECRET:"efefefefefefe11"//should be unique 
-}
