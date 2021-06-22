fe66df46e0ef24be7d3636f398de88ae522470a8
Amrita <amrita@code.com>
2021-06-22 15:51:24 +0530

    make it better

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ app.use(require('./routes/post'))
 app.use(require('./routes/user'))
 
 if(process.env.NODE_ENV=="production"){
-    app.use(express.static('mean/Insta-UI/build'))
+    app.use(express.static('client/build'))
     const path= require('path')
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'mean/Insta-UI','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
 }
 app.listen(PORT,()=>{
